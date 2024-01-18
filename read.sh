f789de983f19b5507e31e80bbf8a8edd1f6048c3
EC2 Default User <ec2-user@ip-172-31-2-58.ap-south-1.compute.internal>
2024-01-18 07:15:26 +0000

    new scripts added

---
+++ read.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#this script is going to take and display an user input
+
+echo "Enter Your Name [Enter]: "
+read name1
+echo "Welcome to India, " $name1 !
---
+++ variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#This script will call the variable inside the script itself
+
+name=rani
+
+echo "$name is my name"
+echo "$name is a IT Engineer"
+echo "$name is 27 years old"
